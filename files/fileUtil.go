52bbffeaa4768391dea6e9a024ff7d935fa481a2
Brian Barasa <briannbig@gmail.com>
2022-11-18 14:57:53 +0300

    Write data to file

--- files/fileUtil.go
+++ files/fileUtil.go
@@ -1,15 +1,29 @@
 package files
 
 import (
+	"fmt"
 	"log"
 	"os"
 )
 
-func CreateFile(FileName string) os.File {
+func CreateFile(FileName string, content string) os.File {
 	file, err := os.Create(FileName)
 
 	if err != nil {
 		log.Fatal("An error occured: ", err.Error())
 	}
+
+	defer file.Close()
+
+	if len(content) == 0 {
+		content = "Empty file. Ready to write"
+	}
+
+	n, err := file.WriteString(content)
+	if err != nil {
+		log.Fatal("Could not write content to file ", err.Error())
+	}
+	log.Output(n, fmt.Sprintf("Content of size %d wrote to file", n))
+
 	return *file
 }
--- main.go
+++ main.go
@@ -8,8 +8,9 @@ import (
 func main() {
 
 	fileName := flag.String("name", "example.txt", "name of the file you want to create")
+	content := flag.String("content", "", "text content you want to write in the file")
 
 	flag.Parse()
 
-	files.CreateFile(*fileName)
+	files.CreateFile(*fileName, *content)
 }
